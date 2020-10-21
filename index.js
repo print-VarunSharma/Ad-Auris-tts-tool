8650b42f37ea44fee336324fff1757621b083c38
Varun Sharma <varun_sharma@live.co.uk>
2020-10-20 22:35:23 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const app = express()
+
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// Pass credentials JSON object into client (For production use ENV Vars)
+const client = new textToSpeech.TextToSpeechClient({
+    credentials: {
+        "type": process.env.TYPE,
+        "project_id": process.env.PROJECT_ID,
+        "private_key_id": process.env.PRIVATE_KEY_ID,
+        "private_key": process.env.PRIVATE_KEY,
+        "client_email": process.env.CLIENT_EMAIL,
+        "client_id": process.env.CLIENT_ID,
+        "auth_uri": process.env.AUTH_URI,
+        "token_uri": process.env.TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+      }
+      
+});
+const fs = require('fs');
+const util = require('util');
+
+
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'));
+app.set('view engine','ejs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/convertText', (req, res) => {
+    convertTextToSpeech(req, res);
+})
+
+async function convertTextToSpeech(req, res) {
+    // Get language code
+    const voiceSelected = req.body.voiceSelect;
+    console.log(voiceSelected)
+
+    // The text to synthesize
+    const text = req.body.text;
+  
+    // Construct the request
+    const request = {
+      input: {text: text},
+      // Select the language and SSML voice gender (optional)
+      voice: {languageCode: ['en-US'], name: voiceSelected.toString()},
+      // select the type of audio encoding
+      audioConfig: {audioEncoding: 'MP3', pitch: req.body.pitch, speakingRate: req.body.speed},
+    };
+  
+    console.log("request " + JSON.stringify(request));
+
+    const fileName = req.body.fileName + '.mp3';
+
+    // Performs the text-to-speech request
+    const [response] = await client.synthesizeSpeech(request);
+    const writeFile = util.promisify(fs.writeFile);
+    await writeFile(fileName, response.audioContent, 'binary');
+    console.log('Audio saved to file: ' + fileName);
+}
+
+app.listen(5000, function () {
+    console.log("Server is listening on Port 5000")
+})
